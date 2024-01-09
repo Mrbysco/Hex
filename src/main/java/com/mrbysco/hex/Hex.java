706f3c10ceec27f5ae8d11e73b719df9e676df0a
Mrbysco <Mrbysco@users.noreply.github.com>
2024-01-09 23:13:33 +0100

    Update to 1.20.4 [build] [publish]

--- src/main/java/com/mrbysco/hex/Hex.java
+++ src/main/java/com/mrbysco/hex/Hex.java
@@ -7,12 +7,11 @@ import com.mrbysco.hex.handler.CheapskateHandler;
 import com.mrbysco.hex.handler.EnchantmentHandler;
 import com.mrbysco.hex.handler.YingYangHandler;
 import com.mrbysco.hex.registry.EnchantmentRegistry;
-import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.neoforged.fml.config.ModConfig;
+import net.neoforged.neoforge.common.NeoForge;
 import org.slf4j.Logger;
 
 @Mod(Hex.MOD_ID)
@@ -20,8 +19,7 @@ public class Hex {
 	public static final String MOD_ID = "hex";
 	public static final Logger LOGGER = LogUtils.getLogger();
 
-	public Hex() {
-		IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+	public Hex(IEventBus eventBus) {
 		ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, HexConfig.commonSpec);
 		eventBus.register(HexConfig.class);
 
